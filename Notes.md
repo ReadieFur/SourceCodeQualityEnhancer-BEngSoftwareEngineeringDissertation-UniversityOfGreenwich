8503a35de161c796364c088a2f72cbac869aa4ed
Tristan Read <60425965+ReadieFur@users.noreply.github.com>
2024-02-07 19:15:20 +0000

    Recreated template (simplified)

---
+++ Notes.md
@@ -0,0 +1,2 @@
+Justifications:
+- Libraries have been used were I see fit as the task of the project is not to solve the problems provided by the libraries but to solve the problems provided by the project, if time permits and requirements change I will reimplement the libraries to solve the problems of the project.
---
+++ src/ReadieFur.SourceAnalyzer.Test.Live/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ReadieFur.SourceAnalyzer.Test.Live
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    internal class Foo
+    {
+        public void Bar()
+        {
+            Console.WriteLine("FooBar");
+        }
+    }
+}
