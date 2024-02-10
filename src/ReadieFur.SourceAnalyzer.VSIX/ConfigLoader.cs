204f7e4fa882f917dec7d618121c7722787a1ad2
Tristan Read <60425965+ReadieFur@users.noreply.github.com>
2024-02-10 20:10:08 +0000

    Got VSPackage working
    I have no clue what I did but I believe it is to do with the ProvideAudoLoad and the .csproj file

---
+++ src/ReadieFur.SourceAnalyzer.VSIX/ConfigLoader.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.Shell;
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ReadieFur.SourceAnalyzer.VSIX
+{
+    //https://stackoverflow.com/questions/54044350/vs2017-vsix-just-created-asyncpackage-is-not-being-instanced
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
+    [Guid("30618704-b18b-4501-8174-2164f88112a5")]
+    [ProvideAutoLoad(UIContextGuids.NoSolution, PackageAutoLoadFlags.BackgroundLoad)]
+    [ProvideAutoLoad(UIContextGuids.SolutionExists, PackageAutoLoadFlags.BackgroundLoad)]
+    public sealed class ConfigLoader : AsyncPackage
+    {
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+        }
+    }
+}
