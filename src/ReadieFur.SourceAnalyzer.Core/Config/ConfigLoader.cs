27a2a73a6e28dabda97ff1b8a923ca41d443d592
Tristan Read <60425965+ReadieFur@users.noreply.github.com>
2024-02-12 01:48:03 +0000

    Misc changes

---
+++ src/ReadieFur.SourceAnalyzer.Core/Config/ConfigLoader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace ReadieFur.SourceAnalyzer.Core.Config
+{
+    public static class ConfigLoader
+    {
+        public static async Task<bool> Load(string path)
+        {
+            try
+            {
+                ConfigRoot config = new DeserializerBuilder()
+                    .WithNamingConvention(UnderscoredNamingConvention.Instance)
+                    .Build()
+                    .Deserialize<ConfigRoot>(path);
+            }
+            catch
+            {
+                return false;
+            }
+
+            return await Task.FromResult(true);
+        }
+    }
+}
