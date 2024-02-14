cd42e1727fb831ad8bde4a22148a220f81e633b8
Tristan Read <60425965+ReadieFur@users.noreply.github.com>
2024-02-14 19:53:29 +0000

    Misc changes

--- src/ReadieFur.SourceAnalyzer.Core/Configuration/ConfigManager.cs
+++ src/ReadieFur.SourceAnalyzer.Core/Configuration/ConfigManager.cs
@@ -1,5 +1,6 @@
 ﻿namespace ReadieFur.SourceAnalyzer.Core.Configuration
 {
+    //The move to a shared project based solution means that I can make the config loader a partial class that needs to be implemented by any other projects that use it.
     public partial class ConfigManager : IConfigManager
     {
         private static readonly ConfigManager _instance = new();
