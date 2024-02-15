8e575fa6d47ac347d860eb1d04803e2a81087a42
Tristan Read <60425965+ReadieFur@users.noreply.github.com>
2024-02-15 01:49:27 +0000

    Created unit test project

---
+++ src/ReadieFur.SourceAnalyzer.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace ReadieFur.SourceAnalyzer.UnitTests
+{
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test1()
+        {
+        }
+    }
+}
