cfa62df55f688629f4fcf3bb401bcfa2ef955968
Tristan Read <60425965+ReadieFur@users.noreply.github.com>
2024-02-15 00:58:23 +0000

    Fall back to simple IPC method

--- src/ReadieFur.SourceAnalyzer.VSIX/Configuration/AConfigManager.cs
+++ src/ReadieFur.SourceAnalyzer.VSIX/Configuration/AConfigManager.cs
@@ -1,17 +1,47 @@
 ﻿using ReadieFur.SourceAnalyzer.Core.Configuration;
 using System;
+using System.IO.MemoryMappedFiles;
+using System.Text;
 
 namespace ReadieFur.SourceAnalyzer.VSIX.Configuration
 {
     internal abstract class AConfigManager : IConfigManager, IDisposable
     {
-        protected virtual object _lock { get; private set; } = new object();
+        protected const int MaxBufferSize = 1024;
 
-        protected virtual ConfigRoot? _cachedConfiguration { get; set; } = null;
+        protected object _lock { get; private set; } = new object();
 
-        public AConfigManager(string ipcName) {}
+        private MemoryMappedFile _sharedMemory;
 
-        public abstract void Dispose();
+        protected MemoryMappedViewAccessor SharedMemoryAccessor;
+
+        protected ConfigRoot? CachedConfiguration { get; set; } = null;
+
+        public AConfigManager(string ipcName)
+        {
+            _sharedMemory = MemoryMappedFile.CreateOrOpen(ipcName + "_memory", MaxBufferSize);
+            SharedMemoryAccessor = _sharedMemory.CreateViewAccessor();
+        }
+
+        public virtual void Dispose()
+        {
+            _sharedMemory.Dispose();
+        }
+
+        protected string ReadSharedMemory()
+        {
+            ushort size = SharedMemoryAccessor.ReadUInt16(54);
+            byte[] buffer = new byte[size];
+            SharedMemoryAccessor.ReadArray(54 + 2, buffer, 0, buffer.Length);
+            return Encoding.ASCII.GetString(buffer);
+        }
+
+        protected void WriteSharedMemory(string data)
+        {
+            byte[] Buffer = Encoding.ASCII.GetBytes(data);
+            SharedMemoryAccessor.Write(54, (ushort)Buffer.Length);
+            SharedMemoryAccessor.WriteArray(54 + 2, Buffer, 0, Buffer.Length);
+        }
 
         public abstract ConfigRoot GetConfiguration();
     }
--- src/ReadieFur.SourceAnalyzer.VSIX/Configuration/HostConfigManager.cs
+++ src/ReadieFur.SourceAnalyzer.VSIX/Configuration/HostConfigManager.cs
@@ -1,10 +1,5 @@
-﻿//#define DO_SOLUTION_EVENT_CHECKS
-
-using CSharpTools.Pipes;
-using static CSharpTools.Pipes.Helpers;
-using ReadieFur.SourceAnalyzer.Core.Configuration;
+﻿using ReadieFur.SourceAnalyzer.Core.Configuration;
 using System;
-using System.Threading.Tasks;
 using EnvDTE;
 using Microsoft.VisualStudio.Shell;
 
@@ -12,114 +7,57 @@ namespace ReadieFur.SourceAnalyzer.VSIX.Configuration
 {
     internal class HostConfigManager : AConfigManager
     {
-        private PipeServerManager _pipeServerManager;
-#if DO_SOLUTION_EVENT_CHECKS
-        private string _currentSolution;
-#endif
+        public HostConfigManager(string ipcName) : base(ipcName) {}
 
-        public HostConfigManager(string ipcName) : base(ipcName)
-        {
-            _pipeServerManager = new PipeServerManager(ipcName, ComputeBufferSizeOf<SSharedConfigMessage>());
-            _pipeServerManager.OnMessage += PipeServerManager_OnMessage;
-        }
-
-        private void PipeServerManager_OnMessage(Guid clientID, ReadOnlyMemory<byte> data)
-        {
-            SSharedConfigMessage message;
-            try { message = Deserialize<SSharedConfigMessage>(data.ToArray()); }
-            catch { return; }
-
-            if (message.RequestConfiguration)
-            {
-                SSharedConfigMessage response = new()
-                {
-                    RequestConfiguration = true,
-                    ConfigurationPath = GetConfigurationPath().ToCharArray()
-                };
-                var a = Serialize(response);
-                var b = ComputeBufferSizeOf<SSharedConfigMessage>();
-                _pipeServerManager.SendMessage(clientID, Serialize(response));
-            }
-        }
-
-        public override void Dispose()
-        {
-            _pipeServerManager.Dispose();
-        }
-
+        //This method is always called before the client method, so we can safely assume that the configuration will be loaded before the client requests it.
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "VSTHRD002:Avoid problematic synchronous waits")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "VSTHRD010:Invoke single-threaded types on Main thread")]
         public override ConfigRoot GetConfiguration()
         {
             lock (_lock)
             {
-#if DO_SOLUTION_EVENT_CHECKS
-                bool solutionChanged = false;
-                ThreadHelper.JoinableTaskFactory.Run(delegate
+                if (CachedConfiguration is not null)
+                    return CachedConfiguration;
+
+                string configurationPath = string.Empty;
+
+                ThreadHelper.JoinableTaskFactory.Run(async delegate
                 {
-                    //The respective visual studio services SHOULD be ready at this point.
                     if (Package.GetGlobalService(typeof(DTE)) is not DTE dte)
                         throw new InvalidOperationException("Failed to get the DTE service.");
 
-                    solutionChanged = _currentSolution != dte.Solution.FileName;
-                    return Task.CompletedTask;
+                    foreach (Project project in dte.Solution.Projects)
+                    {
+                        foreach (ProjectItem item in project.ProjectItems)
+                        {
+                            if (!item.FileNames[1].EndsWith("source-analyzer.yaml"))
+                                continue;
+
+                            if (await YamlLoader.LoadAsync(item.FileNames[1]) is not null)
+                            {
+                                configurationPath = item.FileNames[1];
+                                break;
+                            }
+                        }
+
+                        if (!string.IsNullOrEmpty(configurationPath))
+                            break;
+                    }
                 });
 
-                if (_cachedConfiguration is not null && !solutionChanged)
-                    return _cachedConfiguration;
-#else
-                if (_cachedConfiguration is not null)
-                    return _cachedConfiguration;
-#endif
+                if (string.IsNullOrEmpty(configurationPath))
+                    throw new InvalidOperationException("Failed to find the configuration file.");
 
                 //Load the configuration from the file path.
-                _cachedConfiguration = YamlLoader.LoadAsync(GetConfigurationPath()).Result;
+                CachedConfiguration = YamlLoader.LoadAsync(configurationPath).Result;
 
-                if (_cachedConfiguration is null)
+                if (CachedConfiguration is null)
                     throw new InvalidOperationException("Failed to load the configuration.");
 
-#if DO_SOLUTION_EVENT_CHECKS
-                SSharedConfigMessage message = new()
-                {
-                    SolutionUpdated = true,
-                    ConfigurationPath = GetConfigurationPath()
-                };
-                _pipeServerManager.BroadcastMessage(Serialize(message));
-#endif
+                WriteSharedMemory(configurationPath);
 
-                return _cachedConfiguration;
+                return CachedConfiguration;
             }
-        }
-
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "VSTHRD010:Invoke single-threaded types on Main thread")]
-        private static string GetConfigurationPath()
-        {
-            string configurationPath = string.Empty;
-
-            ThreadHelper.JoinableTaskFactory.Run(async delegate
-            {
-                if (Package.GetGlobalService(typeof(DTE)) is not DTE dte)
-                    throw new InvalidOperationException("Failed to get the DTE service.");
-
-                foreach (Project project in dte.Solution.Projects)
-                {
-                    foreach (ProjectItem item in project.ProjectItems)
-                    {
-                        if (!item.FileNames[1].EndsWith("source-analyzer.yaml"))
-                            continue;
-
-                        if (await YamlLoader.LoadAsync(item.FileNames[1]) is not null)
-                        {
-                            configurationPath = item.FileNames[1];
-                            break;
-                        }
-                    }
-
-                    if (!string.IsNullOrEmpty(configurationPath))
-                        break;
-                }
-            });
-
-            return configurationPath;
         }
     }
 }
