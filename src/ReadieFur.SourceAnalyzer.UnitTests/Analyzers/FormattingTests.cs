c580d31ff0bbd79c4b8ee7417ff9e7561eef7e9a
Tristan Read <60425965+ReadieFur@users.noreply.github.com>
2024-03-20 17:09:40 +0000

    Added curly braces format checker

---
+++ src/ReadieFur.SourceAnalyzer.UnitTests/Analyzers/FormattingTests.cs
@@ -0,0 +1,14 @@
+﻿using ReadieFur.SourceAnalyzer.Core.Analyzers;
+using ReadieFur.SourceAnalyzer.UnitTests.Compatibility;
+using ReadieFur.SourceAnalyzer.UnitTests.TestFiles;
+
+namespace ReadieFur.SourceAnalyzer.UnitTests.Analyzers
+{
+    [CTest]
+    public class FormattingTests : AAnalyzerTester
+    {
+        //[MTest] public async Task BraceLocationAnalyzer() => await TestAnalyzer<BraceLocationAnalyzer>(typeof(CurlyBrace));
+
+        //[MTest] public async Task BraceLocationFixProvider() => await TestFixProvider<BraceLocationAnalyzer, BraceLocationFixProvider>(typeof(CurlyBrace));
+    }
+}
--- src/ReadieFur.SourceAnalyzer.UnitTests/Analyzers/Helpers.cs
+++ src/ReadieFur.SourceAnalyzer.UnitTests/Analyzers/Helpers.cs
@@ -1,25 +1,22 @@
-﻿using Microsoft.CodeAnalysis.CodeFixes;
+﻿using Microsoft;
+using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.Diagnostics;
 
 namespace ReadieFur.SourceAnalyzer.UnitTests.Analyzers
 {
     public abstract class AAnalyzerTester//<TAnalyzer, TFixProvider> where TAnalyzer : DiagnosticAnalyzer, new() where TFixProvider : CodeFixProvider, new()
     {
-        protected async Task<FileInterpreter> CommonTasks(Type sourceType)
-        {
-            await LoadConfiguration();
-            return await FileInterpreter.Interpret(sourceType);
-        }
-
         protected async Task TestAnalyzer<TAnalyzer>(Type sourceType) where TAnalyzer : DiagnosticAnalyzer, new()
         {
-            FileInterpreter file = await CommonTasks(sourceType);
+            await LoadConfiguration();
+            FileInterpreter file = await FileInterpreter.Interpret(sourceType);
             await Verifiers.CSharpAnalyzerVerifier<TAnalyzer>.VerifyAnalyzerAsync(file.AnalyzerInput, file.AnalyzerDiagnostics);
         }
 
         protected async Task TestFixProvider<TAnalyzer, TFixProvider>(Type sourceType) where TAnalyzer : DiagnosticAnalyzer, new() where TFixProvider : CodeFixProvider, new()
         {
-            FileInterpreter file = await CommonTasks(sourceType);
+            await LoadConfiguration();
+            FileInterpreter file = await FileInterpreter.Interpret(sourceType);
             await Verifiers.CSharpCodeFixVerifier<TAnalyzer, TFixProvider>.VerifyCodeFixAsync(file.CodeFixInput, file.CodeFixDiagnostics, file.CodeFixExpected);
         }
     }
