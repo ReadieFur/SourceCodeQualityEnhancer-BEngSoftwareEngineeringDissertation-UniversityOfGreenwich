01040564894d32111bbb845d9658aab772106c05
Tristan Read <60425965+ReadieFur@users.noreply.github.com>
2024-04-24 01:17:44 +0100

    Update Program.cs

--- src/ReadieFur.SourceAnalyzer.ExampleProject/Program.cs
+++ src/ReadieFur.SourceAnalyzer.ExampleProject/Program.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace ReadieFur.SourceAnalyzer.ExampleProject {
 internal class __program {
-private static int my_static_variable=3+4;
 static void Main(string[] args) { // this is the main method of the program
 Console.WriteLine(my_static_variable); // this will print the static variable to the console
 }
+private static int my_static_variable=3+4;
 }
 }
